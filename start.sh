a14fa24ba2e199a8b9221a8f1cac081982e0e9b1
cdnss <32522595+cdnss@users.noreply.github.com>
2025-04-26 00:18:55 +0700

    Update and rename .env.example to start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Jalankan script1.py di latar belakang (&)
+echo "Running script1.py..."
+python3 app.py &
+
+# Jalankan script2.py di latar belakang (&)
+echo "Running script2.py..."
+python bot.py &
+
+# Tunggu hingga semua proses di latar belakang selesai
+# Ini penting agar kontainer tetap berjalan selama script Python berjalan
+wait -n
+
+echo "All scripts finished."
